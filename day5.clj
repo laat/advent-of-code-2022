01426f77d73159240e1a0ab14149c98238035e19
Sigurd Fosseng <sigurd@fosseng.net>
2022-12-07 01:45:14 +0100

    day 5

--- day5.clj
+++ day5.clj
@@ -15,33 +15,26 @@
        (mapv #(zipmap [:n :from :to]
                       (mapv parse-long (re-seq #"\d+" %))))))
 
-(defn move1 [stacks {from :from to :to n :n}]
-  (loop [src (stacks from)
-         dest (stacks to)
-         n n]
-    (if (pos? n)
-      (recur
-       (pop src)
-       (conj dest (peek src))
-       (dec n))
-      (assoc stacks
-             from src
-             to dest))))
-
 ;; part 1
+(defn move1 [stacks {from :from to :to n :n}]
+  (assoc stacks
+         from (drop n (stacks from))
+         to (into (stacks to) (take n (stacks from)))))
+
 (let [input (str/split (slurp "day5.input.txt") #"\n\n")
       stacks (parse-stack (first input))
       moves (parse-moves (second input))]
   (->> (reduce move1 stacks moves)
-       (map peek)
+       (map first)
        (apply str)))
 ;; => "VGBBJCRMN"
 
+;; part 2
 (defn move2 [stacks {from :from to :to n :n}]
   (assoc stacks
          from (drop n (stacks from))
          to (into (stacks to) (reverse (take n (stacks from))))))
-;; part 2
+
 (let [input (str/split (slurp "day5.input.txt") #"\n\n")
       stacks (parse-stack (first input))
       moves (parse-moves (second input))]
